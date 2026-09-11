2d83ce51ee352d24858be7d23d1e6fdda6140858
Michał Cyran <michalcyran77777@gmail.com>
2026-09-11 17:54:37 +0200

    fixes

--- project1/src/main/kotlin/Solution3Expanded.kt
+++ project1/src/main/kotlin/Solution3Expanded.kt
@@ -4,14 +4,19 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
+    var run = true
     runBlocking {
         launch(Dispatchers.IO) {
-            while (true) {
+            while (run) {
                 val input = readln()
 
                 when {
-                    input == "exit" -> cancel()
-                    "[a-zA-z]+ [0-9]+".toRegex().matches(input) -> {
+                    input == "exit" -> {
+                        run = false
+                        cancel()
+                    }
+
+                    "[a-zA-Z]+ [0-9]+".toRegex().matches(input) -> {
                         val values = input.split(" ")
                         val name = values[0]
                         val time = values[1].toInt()
